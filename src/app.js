a743bfdd40582417e3c594f7f787cc6720e0c8b0
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-11-02 01:30:18 +0300

    02.11.2021 başlangıç

---
+++ src/app.js
@@ -0,0 +1,38 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20 //let istediğin değeri alıyor
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11 //const sabit değer
+ 
+//euroDun = 11 //Bunu yapamıyoruz
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi", "Emlak Konut Kredisi", "Kamu Konu Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+    
+}
+console.log("</ul>")
+
+
+// document.write("<ul>")
+// for (let i = 0; i < konutKredileri.length; i++) {
+//     document.write("<li>" + konutKredileri[i] + "</li>")
+    
+// }
+// document.write("</ul>")
+//console.log(konutKredileri)
